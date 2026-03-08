6f25aed2c74f94912138a096cae7a728eec4f92a
Yura Kasyan <33.yurakasyan@gmail.com>
2026-03-08 15:43:29 +0100

    Update package dependencies to include @hookform/resolvers, @react-native-community/datetimepicker, react-hook-form, react-native-get-random-values, uuid, and zod; modify layout to import 'react-native-get-random-values' for enhanced functionality.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-get-random-values';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { queryClient } from '@/api/query-client';
