8eecb440a5de5948cbd45d58b068cd4544f67a1f
Yura Kasyan <33.yurakasyan@gmail.com>
2026-03-12 13:02:05 +0100

    create env.example for environment variables and integrate react-native-config for Firebase initialization.

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,24 @@
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import Config from 'react-native-config';
+// @ts-ignore
+import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: Config.apiKey,
+  authDomain: Config.authDomain,
+  projectId: Config.projectId,
+  storageBucket: Config.storageBucket,
+  messagingSenderId: Config.messagingSenderId,
+  appId: Config.appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+export const db = getFirestore(app);
